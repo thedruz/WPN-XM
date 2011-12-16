f4177cd6746ed31a1b2d5773017b3adabb120252
Jens-Andre Koch <jakoch@web.de>
2011-12-16 18:24:17 +0100

    - added header, footer

---
+++ www/webinterface/templates/footer.php
@@ -0,0 +1,12 @@
+        </div>
+
+        <div id="footer">
+            <hr class="footer-line"/>
+            <p>&copy; 2010-<?php echo date("Y"); ?> by Jens-Andr&#x00E9; Koch Softwaresystemtechnik.
+                <br />
+            </p>
+        </div>
+    </div>
+</div>
+</body>
+</html>
