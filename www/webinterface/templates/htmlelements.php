cbc01355cf8a38d2fb3c03934b48b589404185ab
Jens-Andre Koch <jakoch@web.de>
2012-01-17 17:58:46 +0100

    added fileCounter; welcome message will be displayed 10 times only, then disappears

--- www/webinterface/templates/htmlelements.php
+++ www/webinterface/templates/htmlelements.php
@@ -18,7 +18,55 @@ class HtmlElements
 
     public static function renderWelcome()
     {
-        echo '<h1>Welcome to the WPИ-XM server stack!</h1>';
+        if(self::fileCounter(__DIR__ . '/welcomeMsgCounter.txt', 10) === true)
+        {
+            return;
+        }
+        else
+        {
+            echo '<h1>Welcome to the WPИ-XM server stack!</h1>';
+        }
+    }
+
+    /**
+     * Uses a file for counting the display of the welcome message.
+     *
+     * @param string $file The file containing the counter.
+     * @param int $max_counts The number of times to return false.
+     * @return boolean When the number of max_displays is reached, method will return true; else false;
+     */
+    public static function fileCounter($file, $max_counts)
+    {
+        $max_counts = (int) $max_counts;
+
+        # file to write
+        $file = (string) $file;
+
+        # if file not existing, create and start counting with 1
+        if(is_file($file) === false)
+        {
+            file_put_contents($file, 1);
+        }
+        else
+        {
+            # read file
+            $current = file_get_contents($file);
+
+            # comparison
+            if($current == $max_counts)
+            {
+                return true;
+            }
+
+            # increase counter
+            if($current < $max_counts)
+            {
+                $current++;
+                file_put_contents($file, $current);
+            }
+        }
+
+        return false;
     }
 }
 ?>
