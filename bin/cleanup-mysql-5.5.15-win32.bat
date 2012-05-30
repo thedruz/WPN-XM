f10391d27eb20b06ffe50f02978f9969c7ae96c7
jakoch <jakoch@web.de>
2012-05-30 17:40:41 +0200

    updated processing of /bin folder added more client executables and perl files to be deleted during installation list equals mariadb w32 msi install with database only and without client executables

--- bin/cleanup-mysql-5.5.15-win32.bat
+++ bin/cleanup-mysql-5.5.15-win32.bat
@@ -3,7 +3,7 @@
 
 :: +-------------------------------------------------------------------------
 :: | MySQL 5.5.15 win32 - Stripdown Script
-:: | 
+:: |
 :: | Author: Jens-André Koch
 :: +-----------------------------------------------------------------------<3
 
@@ -13,13 +13,13 @@
 
 IF "%1" == "" (
     ECHO Parameter missing.
-    ECHO Please add the path to mysql.  
+    ECHO Please add the path to mysql.
     GOTO EOF;
 )
 
 :: Check - Parameter is folder
 
-IF NOT EXIST "%1" ( 
+IF NOT EXIST "%1" (
     ECHO Folder "%1" not found.
     ECHO Provide a valid path to the mysql folder.
     GOTO EOF;
@@ -58,7 +58,32 @@ rd /s /q "%MYSQL_DIR%\sql-bench"
 del /s /q "%MYSQL_DIR%\bin\*.pdb"
 
 :: # 2) delete certain executables
+:: This list of executables equals a MariaDB win32 msi install (with only database and no client executables)
 
+del /q /f "%MYSQL_DIR%\bin\aria_chk.exe"
+del /q /f "%MYSQL_DIR%\bin\aria_dump_log.exe"
+del /q /f "%MYSQL_DIR%\bin\aria_ftpdump.exe"
+del /q /f "%MYSQL_DIR%\bin\aria_pack.exe"
+del /q /f "%MYSQL_DIR%\bin\aria_read_log.exe"
+:: keep my_print_defaults.exe
+del /q /f "%MYSQL_DIR%\bin\myisam_ftpdump.exe"
+del /q /f "%MYSQL_DIR%\bin\myisamchk.exe"
+del /q /f "%MYSQL_DIR%\bin\myisamlog.exe"
+del /q /f "%MYSQL_DIR%\bin\myisampack.exe"
+:: keep mysql.exe
+del /q /f "%MYSQL_DIR%\bin\mysqld-embedded.exe"
+:: keep mysql_install_db.exe
+del /q /f "%MYSQL_DIR%\bin\mysql_plugin.exe"
+del /q /f "%MYSQL_DIR%\bin\mysql_tzinfo_to_sql.exe"
+:: keep mysql_upgrade.exe
+:: keep mysql_upgrade_service.exe
+:: keep mysql_upgrade_wizard.exe
+:: keep mysql_admin.exe
+del /q /f "%MYSQL_DIR%\bin\mysqlbinlog.exe"
+:: keep mysqlcheck.exe
+:: keep mysqld.exe
+del /q /f "%MYSQL_DIR%\bin\mysqlimport.exe"
+del /q /f "%MYSQL_DIR%\bin\mysqlshow.exe"
 del /q /f "%MYSQL_DIR%\bin\mysqld-debug.exe"
 del /q /f "%MYSQL_DIR%\bin\mysqltest.exe"
 del /q /f "%MYSQL_DIR%\bin\mysqltest_embedded.exe"
@@ -67,6 +92,14 @@ del /q /f "%MYSQL_DIR%\bin\mysql_client_test_embedded.exe"
 del /q /f "%MYSQL_DIR%\bin\echo.exe"
 del /q /f "%MYSQL_DIR%\bin\mysqlslap.exe"
 del /q /f "%MYSQL_DIR%\bin\replace.exe"
+::keep perror.exe
+
+:: # 3) delte certain perl files
+del /q /f "%MYSQL_DIR%\bin\mysql_convert_table_format.pl"
+del /q /f "%MYSQL_DIR%\bin\mysql_secure_installation.pl"
+del /q /f "%MYSQL_DIR%\bin\mysqld_multi.pl"
+del /q /f "%MYSQL_DIR%\bin\mysqlddumpslow.pl"
+del /q /f "%MYSQL_DIR%\bin\mysqlhotcopy.pl"
 
 :: ############# process the /data folder
 
