fa2d74ceac459a2d9491d55d5eb76aaed9c866cb
jakoch <jakoch@web.de>
2012-06-15 23:37:24 +0200

    added hello_world project

---
+++ www/hello_world/index.php
@@ -0,0 +1,6 @@
+<?php
+   echo 'Welcome to the WPN-XM Server Stack!<br>';
+   echo 'This is just a simple "Hello World" project.<br><br>';
+   echo 'Current working directory is ' . getcwd() . '<br>';
+   echo 'The date and time is ' . date('l jS \of F Y h:i:s A');
+?>
