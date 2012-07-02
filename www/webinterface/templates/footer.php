60ba0dc65afcad7679ec7715c6aa8ea6d9190e11
jakoch <jakoch@web.de>
2012-07-02 15:37:20 +0200

    charset crap

--- www/webinterface/templates/footer.php
+++ www/webinterface/templates/footer.php
@@ -49,7 +49,7 @@
 
             <div style="clear:both">
                 WPN-XM uses <a href="https://github.com/yusukekamiyamane/fugue-icons/">Fugue Icons</a> by <a href="http://p.yusukekamiyamane.com/">Yusuke Kamiyamane</a>.
-                <p>&copy; 2010-<?php echo date("Y"); ?> by Jens-Andr&#x00E9; Koch Softwaresystemtechnik.
+                <p>&#169; 2010-<?php echo date("Y"); ?> by Jens-Andr&#x00E9; Koch Softwaresystemtechnik.
                     <br />
                 </p>
             </div>
