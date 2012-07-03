d14ab5964c10a8c738893162c781ba14711859a5
jakoch <jakoch@web.de>
2012-07-03 10:46:47 +0200

    do not open another modal window, attach content to the existing one do not underline links in buttons

--- www/webinterface/reset_db_pw.php
+++ www/webinterface/reset_db_pw.php
@@ -33,7 +33,7 @@ if($action === 'show')
                 dataType: \'html\',
                 success: function(newHTML, textStatus, jqXHR) {
                   //xy.hideSpinner();
-                  $(newHTML).appendTo(\'div#reset-pw-result\').modal();   
+                  $(newHTML).appendTo(\'div#reset-pw-result\'); //.modal();
                 },
                 // ajax error
               });
@@ -50,34 +50,33 @@ $newPassword = filter_input(INPUT_GET, 'newPassword', FILTER_SANITIZE_STRING);
 
 if($action === "update" && !empty($newPassword))
 {
-    // commands
+    // commands 
     $stop_mariadb = "taskkill /f /IM mysqld.exe 1>nul 2>nul";
-    $mysqld_exe = WPNXM_DIR . "bin\\tools\\runhiddenconsole.exe " . WPNXM_DIR . "bin\\mariadb\\bin\\mysqld.exe";
-    $start_mariadb_change_pw = $mysqld_exe . " --defaults-file=". WPNXM_DIR . 'bin\\mariadb\\my.ini -–skip-grant-tables -u root --init-file=' . WPNXM_DIR . 'bin\\mariadb\\init_passwd_change.txt';
+    $mysqld_exe = WPNXM_DIR . 'bin\\tools\\runhiddenconsole.exe ' . WPNXM_DIR . "bin\\mariadb\\bin\\mysqld.exe";
+    $start_mariadb_change_pw = $mysqld_exe . " --defaults-file=". WPNXM_DIR . 'bin\\mariadb\\my.ini --init-file=' . WPNXM_DIR . 'bin\\mariadb\\init_passwd_change.txt';
     $start_mariadb_normal = $mysqld_exe . " --defaults-file=". WPNXM_DIR . 'bin\\mariadb\\my.ini';
 
     // create the init-file with passwd update query
-    file_put_contents( WPNXM_DIR . 'bin\\mariadb\\init_passwd_change.txt' , "UPDATE mysql.user SET PASSWORD=PASSWORD('$newPassword') WHERE User='root'; FLUSH PRIVILEGES;");
+    file_put_contents( WPNXM_DIR . 'bin\\mariadb\\init_passwd_change.txt', "UPDATE mysql.user SET PASSWORD=PASSWORD('$newPassword') WHERE User='root'; FLUSH PRIVILEGES;");
 
     // start mysqld again with init-file to change password
-    passthru($stop_mariadb);
-    passthru($start_mariadb_change_pw); 
-    sleep("5");
-    passthru($stop_mariadb);
-    passthru($start_mariadb_normal);
-    sleep("5");
+    exec($stop_mariadb);
+    exec($start_mariadb_change_pw); 
+    sleep("2");
+
+    exec($stop_mariadb);
+    exec($start_mariadb_normal);
+    sleep("2");
 
     unlink(WPNXM_DIR . 'bin\\mariadb\\init_passwd_change.txt'); 
 
-    $return = "Password changed SUCCESSFULLY.";
-
     $connection = new mysqli("localhost", "root", $newPassword, "mysql");
 
-    if($connection->connect_errno) {
-        $return .= '<div class="error">Database Connection with new password FAILED.<br/>(MySQL ["' . $connection->connect_errno . '"]"' . $connection->connect_error . '")';
+    if(mysqli_connect_errno()) {
+        $return = '<div class="error">Database Connection with new password FAILED.<br/>(MySQL ["' . mysqli_connect_errno() . '"]"' . mysqli_connect_error() . '")';
     }
     else {
-        $return .= '<div class="success">Database Connection with new password SUCCESSFUL.';
+        $return = '<div class="success">Password changed SUCCESSFULLY.';
     }
 
     $return .= '<br/><a class="aButton" rel="modal:close" href="#">Close</a></div>';
