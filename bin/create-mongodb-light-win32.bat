8fa6272cb294a717ae54630ef4b181faa7d98dde
jakoch <jakoch@web.de>
2013-02-02 03:40:05 +0100

    added MongoDb stripdown script

---
+++ bin/create-mongodb-light-win32.bat
@@ -0,0 +1,53 @@
+@echo off
+:: Change this to ON when debugging this batch file.
+
+:: +-------------------------------------------------------------------------
+:: | MongoDB 2.2.1 win32 - Stripdown Script for WPN-XM Server Stack.
+:: | http://wpn-xm.org/
+:: | Author: Jens-André Koch
+:: +-----------------------------------------------------------------------<3
+
+:: ############# Accept "path to mongodb" as first parameter
+
+:: Because of possible spaces in the folder name,
+:: one must use quotes on the argument, like so
+:: create-mongodb-light-win32.bat "c:\program files\somewhere"
+
+:: Check - Parameter is not empty
+
+IF "%~1" == "" (
+    ECHO Parameter missing.
+    ECHO Please add the path to mysql.
+    GOTO EOF;
+)
+
+:: Check - Parameter is folder
+
+IF NOT EXIST "%~1" (
+    ECHO Folder "%~1" not found.
+    ECHO Provide a valid path to the mysql folder.
+    GOTO EOF;
+)
+
+:: Check - Parameter is the mysql folder
+
+IF NOT EXIST "%~1\bin\mongo.exe" (
+    ECHO Folder "%~1" exists, but is not the mysql folder.
+    GOTO EOF;
+)
+
+:: Ok, now we know where mysql resides...
+
+SET MONGO_DIR=%~1
+
+echo %MONGO_DIR%
+
+:: ############# process the /bin folder
+
+:: # 1) delete pdb files (windows crashdumps debug files)
+
+del /s /q "%MONGDO_DIR%\bin\*.pdb"
+
+:: ############# compress executables with UPX
+
+:: @todo
