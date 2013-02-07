ae24c21451bd8b33efaa05fd43a10114c48e0fd2
jakoch <jakoch@web.de>
2013-02-08 00:07:15 +0100

    updated paths, create certs folder, display some hints

--- bin/generate-certificate.bat
+++ bin/generate-certificate.bat
@@ -5,4 +5,18 @@ cls
 TITLE WPN-XM Server Stack for Windows - Generate SSL Certificate
 
 pushd %~dp0
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout cert.key -out cert.pem -config ./openssl.cnf
+
+if not exist "%~dp0\certs" (
+    echo .
+    echo Creating Certs folder... %~dp0\certs
+    echo .
+    md %~dp0\certs
+)
+
+
+%~dp0\bin\openssl.exe req -x509 -nodes -days 365 -newkey rsa:2048 -keyout certs\cert.key -out certs\cert.pem -config %~dp0\bin\openssl.cfg
+
+echo .
+echo "Find your new SSL Certificate in %~dp0\certs"
+echo .
+pause
