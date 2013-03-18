7227b84dc03aa8fc4448f0756c56656b8989a56a
jakoch <jakoch@web.de>
2013-03-18 18:05:38 +0100

    fixes #97

--- bin/install-phpunit.bat
+++ bin/install-phpunit.bat
@@ -12,21 +12,25 @@ cls
 :: set window title
 TITLE WPN-XM Server Stack for Windows - phpUnit Installation Script
 
+cd PEAR
+
 :: upgrade pear
 
-pear upgrade pear
+call pear upgrade-all
 
 :: register channels
 
-pear channel-discover pear.phpunit.de
-pear channel-discover components.ez.no
-pear channel-discover pear.symfony-project.com
+call pear channel-discover pear.phpunit.de
+call pear channel-discover components.ez.no
+call pear channel-discover pear.symfony-project.com
 
 :: update all channels
 
-pear update-channels
+call pear update-channels
 
 :: install packages
 
-pear clear-cache
-pear install --alldeps --force phpunit/PHPUnit
+call pear clear-cache
+call pear install --alldeps --force phpunit/PHPUnit
+
+cd ..
