e56df83dbb011fb83c058f3c6c9a721c4764f932
jakoch <jakoch@web.de>
2013-03-30 00:06:12 +0100

    base for a relocation script

---
+++ startfiles/relocate.bat
@@ -0,0 +1,16 @@
+@echo off
+
+:: set window title
+TITLE WPN-XM Serverpack - Daemon Status Monitor
+
+:: adjust nginx.conf (include path)
+
+:: adjust php.ini
+
+:: adjust mariadb my.cnf
+
+:: delete all temporary files
+del /s /q temp
+
+echo.
+echo WPN-XM was successfully relocated!
