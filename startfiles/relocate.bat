0d76d05a96fd144e9f57cbe575301e5c25c1e8c0
jakoch <jakoch@web.de>
2013-04-05 20:38:23 +0200

    comments on relocate

--- startfiles/relocate.bat
+++ startfiles/relocate.bat
@@ -1,13 +1,14 @@
 @echo off
 
 :: set window title
-TITLE WPN-XM Serverpack - Daemon Status Monitor
+TITLE WPN-XM Serverpack - Relocate
 
 :: adjust nginx.conf (include path)
 
 :: adjust php.ini
 
-:: adjust mariadb my.cnf
+:: adjust mariadb my.ini
+:: log-error=$PATH/logs/mariadb_error.log
 
 :: delete all temporary files
 del /s /q temp
