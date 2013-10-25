c5de6d50821f4cf277d9a7e270fe99986b13478e
jakoch <jakoch@web.de>
2013-10-25 11:12:50 +0200

    renamed "all-in-one" menu entry to "non-web"

--- build-menu.bat
+++ build-menu.bat
@@ -12,12 +12,13 @@ TITLE WPN-XM Server Stack for Windows - Build Script!
 
 :SHOWMENU
 cls
+echo.
 echo             WPN-XM Build Menu
 echo             =================
 echo.
 echo   [1] Start "One-Click Build"
-echo   [2] Download Components for "All-In-One" Installation Wizard
-echo   [3] Build the "All-In-One" Installation Wizard
+echo   [2] Download Components for the "not-web" Installation Wizards
+echo   [3] Build the "not-web" Installation Wizards
 echo   [x] Quit
 echo.
 
@@ -42,13 +43,13 @@ goto SHOWMENU
 goto END
 
 :OPTION2
-    echo Downloading all Components for the All-In-One Installation Wizard
+    echo Downloading all Components for the "not-web" Installation Wizards
     call bin\nant\bin\nant.exe -buildfile:build.xml download-components
 goto END
 
 :OPTION3
-    echo Rebuild AllInOne Installation Wizard (use existing Downloads)
-    call bin\nant\bin\nant.exe -buildfile:build.xml compile-wpnxm-allinone-installer-no-download
+    echo Building "not-web" Installation Wizards (use existing Downloads)
+    call bin\nant\bin\nant.exe -buildfile:build.xml compile-wpnxm-allinone-installer-no-download compile-wpnxm-bigpack-installer-no-download
 goto END
 
 :END
