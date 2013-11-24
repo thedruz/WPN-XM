9871044ca7f2d04644cbdfdf81d70f219040a09a
jakoch <jakoch@web.de>
2013-11-24 02:19:43 +0100

    added menu entry for building the server control panel

--- build-menu.bat
+++ build-menu.bat
@@ -19,6 +19,7 @@ echo.
 echo   [1] Start "One-Click Build"
 echo   [2] Download Components for the "not-web" Installation Wizards
 echo   [3] Build the "not-web" Installation Wizards
+echo   [4] Build the "Server-Control-Panel" (Tray Application)
 echo   [x] Quit
 echo.
 
@@ -32,24 +33,38 @@ set /p input="Your selection: "
 if %input%==1 goto OPTION1
 if %input%==2 goto OPTION2
 if %input%==3 goto OPTION3
+if %input%==4 goto OPTION4
 IF %input%==x goto QUIT
 goto SHOWMENU
 
 :: handlers
 
 :OPTION1
+    echo.
     echo One-Click Build
+    echo.
     call build.bat
 goto END
 
 :OPTION2
+    echo.
     echo Downloading all Components for the "not-web" Installation Wizards
+    echo.
     call bin\nant\bin\nant.exe -buildfile:build.xml download-components
 goto END
 
 :OPTION3
+    echo.
     echo Building "not-web" Installation Wizards (use existing Downloads)
+    echo.
     call bin\nant\bin\nant.exe -buildfile:build.xml compile-wpnxm-allinone-installer-no-download compile-wpnxm-bigpack-installer-no-download compile-wpnxm-lite-installer-no-download
+goto END
+
+:OPTION4
+    echo.
+    echo Building the "Server-Control-Panel" (Tray Application)
+    echo.
+    call bin\nant\bin\nant.exe -buildfile:build.xml compile-server-control-panel
 goto END
 
 :END
