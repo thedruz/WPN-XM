a4f8e20471871357cd552aa5365baa504a8b0a88
jakoch <jakoch@web.de>
2014-08-27 16:55:30 +0200

    added pickle shortcut batch, issue #229

---
+++ startfiles/pickle.bat
@@ -0,0 +1,12 @@
+@echo off
+
+:: +-------------------------------------------------------------------------
+:: |
+:: | WPN-XM Server Stack - Pickle CLI Shortcut (Global Installation)
+:: |
+:: +-----------------------------------------------------------------------<3
+
+:: call pickle
+"%~dp0php.exe" "%~dp0pickle.phar" %*
+
+pause
