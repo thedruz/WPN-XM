75031ed1acef310149b9ac902fe1fab681b0debe
jakoch <jakoch@web.de>
2014-12-20 04:01:18 +0100

    added implicit repair on stop for mongodb stop command

--- startfiles/stop-mongodb.bat
+++ startfiles/stop-mongodb.bat
@@ -10,4 +10,6 @@ cls
 
 %cd%\bin\mongodb\bin\mongo --eval "db.getSiblingDB('admin').shutdownServer()"
 
+%cd%\bin\mongodb\bin\mongod --config "%cd%\bin\mongodb\mongodb.conf" --logpath "%cd%\logs\mongodb.log" --dbpath "%cd%\bin\mongodb\data\db" --repair
+
 pause
