56e6ab769e5817e4c6773d7ff184924d871c53e0
jakoch <jakoch@web.de>
2015-01-27 23:56:30 +0100

    diffuse edit helpers for "row editing over multiple installers"

---
+++ installers/diff-standard.bat
@@ -0,0 +1,3 @@
+@echo off
+
+"C:\Program Files (x86)\Diffuse\diffuse" standard-php54-w32.iss standard-php55-w32.iss standard-php55-w64.iss standard-php56-w32.iss standard-php56-w64.iss
---
+++ installers/diff-webinstaller-debug.bat
@@ -0,0 +1,3 @@
+@echo off
+
+"C:\Program Files (x86)\Diffuse\diffuse" webinstaller-debug-php54-w32.iss webinstaller-debug-php55-w32.iss webinstaller-debug-php55-w64.iss webinstaller-debug-php56-w32.iss webinstaller-debug-php56-w64.iss
