319a2549bace57002509404b954b100716ed90b1
jakoch <jakoch@web.de>
2015-03-06 16:12:43 +0100

    added copyright [skip ci]

--- bin/stripdown.php
+++ bin/stripdown.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * WPИ-XM Server Stack
+ * Copyright © 2010 - 2015 Jens-André Koch <jakoch@web.de>
+ * http://wpn-xm.org/
+ *
+ * This source file is subject to the terms of the MIT license.
+ * For full copyright and license information, view the bundled LICENSE file.
+ */
 
 /**
  *  Stripdown Scripts
