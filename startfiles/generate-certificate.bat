dc1b92506286ff4c73ac667eceb80c538199bd14
jakoch <jakoch@web.de>
2015-03-09 23:38:09 +0100

    the files resides in /bin/openssl and not in the root folder of the server stack, issue https://github.com/WPN-XM/WPN-XM/issues/332

--- startfiles/generate-certificate.bat
+++ startfiles/generate-certificate.bat
@@ -13,18 +13,18 @@ TITLE WPN-XM Server Stack for Windows - Generate SSL Certificate
 
 pushd %~dp0
 
-if not exist "%~dp0\certs" (
+if not exist "%~dp0certs" (
     echo .
-    echo Creating Certs folder... %~dp0\certs
+    echo Creating Certs folder... %~dp0certs
     echo .
-    md %~dp0\certs
+    md %~dp0certs
 )
 
 
-%~dp0\bin\openssl.exe req -x509 -nodes -days 365 -newkey rsa:2048 -keyout certs\cert.key -out certs\cert.pem -config %~dp0\bin\openssl.cfg
+openssl.exe req -x509 -nodes -days 365 -newkey rsa:2048 -keyout certs\cert.key -out certs\cert.pem -config openssl.cfg
 
 echo .
-echo "Find your new SSL Certificate in %~dp0\certs"
+echo "Find your new SSL Certificate in %~dp0certs"
 echo .
 
 pause
