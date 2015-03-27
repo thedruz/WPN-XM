356f95c0b710e34869ee3932f5aa6a55569bcd40
jakoch <jakoch@web.de>
2015-03-28 00:56:04 +0100

    ensure phar files exists, before executing self-update [skip ci]

--- startfiles/update-phars.bat
+++ startfiles/update-phars.bat
@@ -6,10 +6,16 @@
 :: |
 :: +-----------------------------------------------------------------------<3
 
-"%~dp0php.exe" "%~dp0composer.phar" self-update
+IF EXIST "%~dp0composer.phar" (
+    "%~dp0php.exe" "%~dp0\composer.phar" self-update
+)
 
-"%~dp0php.exe" "%~dp0phpunit.phar" --self-update
+IF EXIST "%~dp0phpunit.phar" (
+    "%~dp0php.exe" "%~dp0\phpunit.phar" --self-update
+)
 
-"%~dp0php.exe" "%~dp0php-cs-fixer.phar" self-update
+IF EXIST "%~dp0php-cs-fixer.phar" (
+    "%~dp0php.exe" "%~dp0\php-cs-fixer.phar" self-update
+)
 
 pause
