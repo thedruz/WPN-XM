18a756ff13dba83af75a661b8bfb21bb69c41575
jakoch <jakoch@web.de>
2015-06-28 16:24:08 +0200

    remove ph54 from diff helper scripts [skip ci]

--- installers/diff-webinstaller.bat
+++ installers/diff-webinstaller.bat
@@ -1,3 +1,3 @@
 @echo off
 
-"C:\Program Files (x86)\Diffuse\diffuse" webinstaller-php54-w32.iss webinstaller-php55-w32.iss webinstaller-php55-w64.iss webinstaller-php56-w32.iss webinstaller-php56-w64.iss
+"C:\Program Files (x86)\Diffuse\diffuse" webinstaller-php55-w32.iss webinstaller-php55-w64.iss webinstaller-php56-w32.iss webinstaller-php56-w64.iss
