dbbab04212dad0b7d19a3188bfb09f711605ff26
jakoch <jakoch@web.de>
2015-07-22 03:16:33 +0200

    changed order of files in diffuse multi-way merge [skip ci]

--- installers/diff-crossover.bat
+++ installers/diff-crossover.bat
@@ -1,3 +1,3 @@
 @echo off
 
-"C:\Program Files (x86)\Diffuse\diffuse" full-php55-w32.iss standard-php55-w32.iss lite-php55-w32.iss webinstaller-php55-w32.iss
+"C:\Program Files (x86)\Diffuse\diffuse" webinstaller-php55-w32.iss full-php55-w32.iss standard-php55-w32.iss lite-php55-w32.iss
