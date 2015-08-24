d897bffd46b95218a33ebfb12a56331be100b7cb
jakoch <jakoch@web.de>
2015-08-24 22:21:43 +0200

    adjusted console.bat to detect Git for Windows v2.5.0+ and open git-cmd correctly [skip ci]

--- startfiles/console.bat
+++ startfiles/console.bat
@@ -21,10 +21,18 @@ SET HIDECONSOLE=%~dp0bin\tools\RunHiddenConsole.exe
 :: when "git" is installed, we can start ConEmu with a split screen.
 :: On the top    - windows command prompt
 :: On the bottom - git bash
+
 IF EXIST "%~dp0\bin\git\git-bash.bat" (
+	:: an old version of msysgit
 	%HIDECONSOLE% %CONEMU% -cmdlist ^
 	cmd.exe -cur_console:fn:t:"Cmd":C:"%~dp0bin\conemu\terminal.ico" ^|^|^| ^
 	%~dp0bin\git\git-bash.bat -cur_console:sV:t:"GitBash":C:"%~dp0bin\git\etc\git.ico"
+) ELSE IF EXIST "%~dp0\bin\git\git-cmd.exe" (
+    :: Git for Windows v2.5.0+
+	%HIDECONSOLE% %CONEMU% -cmdlist ^
+	cmd.exe -cur_console:fn:t:"Cmd":C:"%~dp0bin\conemu\terminal.ico" ^|^|^| ^
+	%~dp0bin\git\git-cmd.exe -cur_console:sV:t:"GitBash":C:"%~dp0bin\git\usr\share\git\git.ico"
 ) ELSE (
+    :: git not installed
 	%HIDECONSOLE% %CONEMU%
 )
