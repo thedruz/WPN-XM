7fe2762344b7896115e7108332b5bd910e2fc839
jakoch <jakoch@web.de>
2015-09-30 14:26:06 +0200

    exec "bin/composer/composer.phar" from "bin/php/php.exe", issue https://github.com/WPN-XM/WPN-XM/issues/486 [skip ci]

--- startfiles/composer.bat
+++ startfiles/composer.bat
@@ -6,7 +6,13 @@
 :: |
 :: +-----------------------------------------------------------------------<3
 
+:: set "\bin\composer"
+SET COMPOSER_HOME="%~dp0"
+
+:: set "\bin\composer\cache"
+SET COMPOSER_CACHE_DIR="%~dp0cache"
+
 :: call composer
-"%~dp0php.exe" "%~dp0composer.phar" %*
+echo "%~dp0..\php\php.exe" "%~dp0composer.phar" %*
 
 pause
