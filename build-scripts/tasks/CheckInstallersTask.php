9ae340a3e7f316c8a6772d446d98a178b1faa8cb
jakoch <jakoch@web.de>
2016-02-24 12:44:25 +0100

    fix array_diff on iss/exe build check [skip ci]

--- build-scripts/tasks/CheckInstallersTask.php
+++ build-scripts/tasks/CheckInstallersTask.php
@@ -27,26 +27,38 @@ class CheckInstallersTask extends Task
     {
         $this->log('Checking Installers..');
 
-        $installer_scripts = array_map('basename', glob($this->buildFolder . '/*.iss', GLOB_BRACE));
-        $installer_executables = array_map('basename', glob($this->buildFolder . '/*.exe', GLOB_BRACE));
+        $installerScripts     = array_map('basename', glob($this->buildFolder . '/*.iss', GLOB_BRACE));
+        $installerExecutables = array_map('self::mapExecutables', glob($this->buildFolder . '/*.exe', GLOB_BRACE));
+        $missing_installers   = array_diff($installerScripts, $installerExecutables);
 
-        $missing_installer_executables = array_diff($installer_scripts, $installer_executables);
-
-        if(count($missing_installer_executables) > 0) {
+        if(count($missing_installers) > 0) {
             echo "\033[01;31m";
             echo "The following installer executables were not compiled:\n";
-            foreach($missing_installer_executables as $exe) {
-               echo "  - \"$exe\".exe \n";
+            foreach($missing_installers as $installer) {
+               echo '  - '.$installer."\n";
             }
             echo "Please check the InnoSetup script files for errors!\n";
-            echo "\033[0m";
-        } else {
-            $total_installers_build = count($installer_executables);
-            if($total_installers_build > 0) {
-                $this->log('Successfully build ' . $total_installers_build . ' installers!');
-            } else {
-                $this->log('0 installers build! Grrr. Check the log and the compile task.');
-            }
+            echo "\033[0m\n";
         }
+
+        $total_installers_build = count($installerExecutables);
+        if($total_installers_build > 0) {
+            $this->log('Successfully build installers for the following ' . $total_installers_build . ' scripts:');
+            foreach($installerExecutables as $installer) {
+               echo '  - '.$installer."\n";
+            }
+        } else {
+            $this->log('0 installers build! Grrr. Check the log and the compile task.');
+        }
+    }
+
+    public static function mapExecutables($item)
+    {
+        $item  = basename($item);
+        $item  = substr($item, 12);
+        $item  = str_replace(['-Setup','.exe'], ['', '.iss'], $item);
+        $item  = strtolower($item);
+
+        return $item;
     }
 }
