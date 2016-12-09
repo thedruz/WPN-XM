19b60f6b61266cf87b5028dda95c8c528c1592be
jakoch <jakoch@web.de>
2016-12-10 00:46:14 +0100

    update diff scripts for 7.1, issue https://github.com/WPN-XM/WPN-XM/issues/619 [skip ci]

--- installers/diff-standard.bat
+++ installers/diff-standard.bat
@@ -1,3 +1,3 @@
 @echo off
 
-"C:\Program Files (x86)\Diffuse\diffuse" standard-php56-w32.iss standard-php56-w64.iss standard-php70-w32.iss standard-php70-w64.iss
+"C:\Program Files (x86)\Diffuse\diffuse" standard-php56-w32.iss standard-php56-w64.iss standard-php70-w32.iss standard-php70-w64.iss standard-php71-w32.iss standard-php71-w64.iss
