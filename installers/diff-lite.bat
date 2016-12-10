ef75a15bb2bdc2d6ba036ea07544b47b1aca7bb2
jakoch <jakoch@web.de>
2016-12-10 15:39:23 +0100

    add literc diff script adjust crossover script [skip ci]

--- installers/diff-lite.bat
+++ installers/diff-lite.bat
@@ -1,3 +1,3 @@
 @echo off
 
-"C:\Program Files (x86)\Diffuse\diffuse" lite-php56-w32.iss lite-php56-w64.iss lite-php70-w32.iss lite-php70-w64.iss literc-php71-w32.iss literc-php71-w64.iss
+"C:\Program Files (x86)\Diffuse\diffuse" lite-php56-w32.iss lite-php56-w64.iss lite-php70-w32.iss lite-php70-w64.iss lite-php71-w32.iss lite-php71-w64.iss
---
+++ installers/diff-literc.bat
@@ -0,0 +1,3 @@
+@echo off
+
+"C:\Program Files (x86)\Diffuse\diffuse" literc-php71-w32.iss literc-php71-w64.iss
