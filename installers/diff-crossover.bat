f4338a21e185250b4c047f586f272d77a5c54eed
jakoch <jakoch@web.de>
2018-09-27 18:21:34 +0200

    remove php56 installers

--- installers/diff-crossover.bat
+++ installers/diff-crossover.bat
@@ -1,3 +1,3 @@
 @echo off
 
-"C:\Program Files (x86)\Diffuse\diffuse" webinstaller-php56-w64.iss full-php56-w64.iss standard-php56-w64.iss lite-php56-w64.iss literc-php71-w64.iss
+"C:\Program Files (x86)\Diffuse\diffuse" webinstaller-php70-w64.iss full-php56-w64.iss standard-php56-w64.iss lite-php56-w64.iss literc-php71-w64.iss
