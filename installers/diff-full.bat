544b87e6d11619014451f19f875f243305bf2095
jakoch <jakoch@web.de>
2019-01-08 00:19:31 +0100

    added installers for PHP7.3 & adjusted diff batch files, issue https://github.com/WPN-XM/WPN-XM/issues/668 [skip ci]

--- installers/diff-full.bat
+++ installers/diff-full.bat
@@ -1,3 +1,3 @@
 @echo off
 
-"C:\Program Files (x86)\Diffuse\diffuse" full-php70-w64.iss full-php71-w64.iss full-php72-w64.iss
+"C:\Program Files (x86)\Diffuse\diffuse" full-php70-w64.iss full-php71-w64.iss full-php72-w64.iss full-php73-w64.iss
